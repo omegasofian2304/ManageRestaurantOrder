a5660f219dff78e552eeee9495abf9bfcb14934d
milodelasuisse <pk26rlr@eduvaud.ch>
2026-03-10 16:10:30 +0100

    fix(order): fix create order controllers

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -7,28 +7,29 @@ export const create = async (req, res,next) => {
         if (!clientName  === undefined) {
             return res.status(400).json({ error: 'clientName order required' })
         }
-        if (!served  === undefined) {
-            return res.status(400).json({ error: ' served  order required' })
+        else if (clientName) {
+            if (clientName.length > 45) {
+                return res.status(400).json({error: 'clientName must be less than 45 characters'})
+            }
         }
-        if (!price === undefined) {
-            return res.status(400).json({ error: 'price order required' })
+        if (!served  === undefined) {
+            return res.status(400).json({ error: 'served  order required' })
         }
         if (!employee_id === undefined) {
             return res.status(400).json({ error: 'employee_id order required' })
         }
-        if (typeof price !== 'number' || price <= 0) {
-            return res.status(400).json({ error: 'price must be a positive number' })
-        }
-        if (typeof served !== 'boolean') {
-            return res.status(400).json({ error: 'served must be a boolean' })
-        }
-        if (typeof employee_id !== 'number' || price <= 0) {
+        else if (typeof employee_id !== 'number' || price <= 0) {
             return res.status(400).json({ error: 'employee_id must be a positive number' })
         }
-        if (clientName) {
-            if (clientName.length > 45) {
-                return res.status(400).json({error: 'clientName must be less than 45 characters'})
-            }
+        else if (typeof served !== 'boolean') {
+            return res.status(400).json({ error: 'served must be a boolean' })
+        }
+        if (!price === undefined) {
+            return res.status(400).json({ error: 'price order required' })
+
+        }
+        else if (typeof price !== 'number' || price <= 0) {
+            return res.status(400).json({ error: 'price must be a positive number' })
         }
 
         const order = { clientName, served, price, employee_id }
