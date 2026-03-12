e29548c380a3c2e6905656e7202333e576e8609c
Rodrigo <pi40qdk@eduvaud.ch>
2026-03-12 09:43:14 +0100

    Feature/us 14 change order status (#54)
    * feat(meal): add show all meals endpoint
    
    * feat: Add the endpoint "change order status"
    
    * fix: resolve conflicts

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -4,14 +4,17 @@ Date : 04.03.2026
 Title : orderRepository.js
 Desc : File containing all route for the order table
 */
-
-import express from "express";
+import {serveOrder} from "../controllers/orderController.js";
+import { Router } from "express";
 import {create, getAllOrders} from "../controllers/orderController.js";
 
 
-const router = express.Router();
+const router = Router();
 
 router.post("/", create);
+
 router.get("/", getAllOrders)
+
+router.patch('/:id/status', serveOrder);
 
 export default router;
--- src/services/orderServices.js
+++ src/services/orderServices.js
@@ -5,13 +5,29 @@ Title : orderRepository.js
 Desc : Business logic for the order table
 */
 
-import { createOrder as createOrderRepository } from "../repositories/orderRepository.js";
 import{findAllOrder as findAllOrders} from "../repositories/orderRepository.js";
+import { createOrder as createOrderRepository, serveOrder as serveOrderRepository } from "../repositories/orderRepository.js";
+import { findOrderById as findOrderByIdRepository } from "../repositories/orderRepository.js";
+import {findOrderWithMeals as findOrderWithMealsRepository} from "../repositories/orderRepository.js";
+
 
 export const createOrder = async (data) => {
     return createOrderRepository(data);
 }
+
 export const findAllOrder = async () => {
-    return findAllOrders();
-    }
-;
+        return findAllOrders();
+    };
+
+export const findOrderById = async (id) => {
+    return findOrderByIdRepository(id);
+}
+
+
+export const findOrderWithMeals = async (id) => {
+    return findOrderWithMealsRepository(id);
+}
+
+export const serveOrder = async (id) => {
+    return await serveOrderRepository(id);
+};
